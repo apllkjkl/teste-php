34b081420ce219c29bb5db812e1ed17f0e9aaf42
apkjkl <110535753+apllkjkl@users.noreply.github.com>
2024-03-11 06:57:11 -0300

    a
    aa

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <div class="header">
+            <h1>gerenciador de tarefas</h1>
+        </div>
+        <div class="form">
+            <form action="" method="get">
+                <label for="task-name"></label>
+                <input type="text" name="task name" placeholder="Nome da tarefa">
+                <button type="submit">Cadastrar Produto</button>
+        </div>
+        <div class="separador">
+
+        </div>
+        <div class="lista">
+            <ul>
+                <li>Tarefa 1</li>
+                <li>Tarefa 2</li>
+                <li>Tarefa 3</li>
+            </ul>
+
+        </div>
+        <div class="rodape">
+            <p>Desenvolvido por apkjkl</p>
+
+        </div>
+    </div>
+</body>
+</html>
